c0d6bceb18b7773f70bc6731749eda319870c7df
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2020-02-13 20:55:55 +0530

    some minor changes made

--- src/q1/run.py
+++ src/q1/run.py
@@ -12,25 +12,16 @@ xDict , yDict = np.hsplit(rawDict, 2)
 xTrain, xTest, yTrain, yTest = train_test_split(xDict, yDict, test_size=0.1, random_state=20)
 
 dx , dy = distribute(xTrain, yTrain)
+train_x = dx[5]
+train_y = dy[5]
 
-for i in range(0, 10):
-    plt.scatter(xDict, yDict, label='rand', s=1)
-    dx[i] = dx[i][:, np.newaxis]
-    dy[i] = dy[i][:, np.newaxis]
-    poly = PolynomialFeatures(degree=17)
-    x_poly = poly.fit_transform(dx[i])
-    model = LinearRegression()
-    model.fit(x_poly, dy[i])
-    y_poly_pred = model.predict(x_poly)
-    sort_axis = operator.itemgetter(0)
-    sorted_zip = sorted(zip(dx[i],y_poly_pred), key=sort_axis)
-    dx[i], y_poly_pred = zip(*sorted_zip)
-    plt.plot(dx[i], y_poly_pred, color='m')
-    plt.show()
-    # plt.plot(dx[i], y_poly_pred)
-    # plt.show()
-# bias, variance = train(dx , dy, xTest, yTest)
+poly_reg = PolynomialFeatures(degree = 3)
+x_poly = poly_reg.fit_transform(train_x)
+pol_reg = LinearRegression()
+pol_reg.fit(x_poly, train_y)
 
+pred_y = pol_reg.predict(poly_reg.fit_transform(xTest))
+plt.plot(xTest, pred_y)
 plt.scatter(xDict, yDict, label='rand', s=1)
 plt.show()
 
