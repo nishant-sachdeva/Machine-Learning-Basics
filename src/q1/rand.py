8a9926e3a34acfa2f3b267fdc33594f02f48d861
starc52 <cstarpowful@gmail.com>
2020-02-13 17:41:45 +0530

    lol

---
+++ src/q1/rand.py
@@ -0,0 +1,39 @@
+import operator
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import PolynomialFeatures
+
+np.random.seed(0)
+x = 2 - 3 * np.random.normal(0, 1, 20)
+y = x - 2 * (x ** 2) + 0.5 * (x ** 10) + np.random.normal(-3, 3, 20)
+
+# transforming the data to include another axis
+x = x[:, np.newaxis]
+y = y[:, np.newaxis]
+
+polynomial_features= PolynomialFeatures(degree=10)
+x_poly = polynomial_features.fit_transform(x)
+
+model = LinearRegression()
+model.fit(x_poly, y)
+y_poly_pred = model.predict(x_poly)
+
+# plt.plot(x, y_poly_pred)
+# plt.show()
+
+rmse = np.sqrt(mean_squared_error(y,y_poly_pred))
+r2 = r2_score(y,y_poly_pred)
+print(rmse)
+print(r2)
+
+plt.scatter(x, y, s=10)
+# sort the values of x before line plot
+sort_axis = operator.itemgetter(0)
+sorted_zip = sorted(zip(x,y_poly_pred), key=sort_axis)
+x, y_poly_pred = zip(*sorted_zip)
+plt.plot(x, y_poly_pred, color='m')
+plt.show()
